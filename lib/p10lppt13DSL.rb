13f800a35b1ab1ad2e72275835056d118d8fe4fc
Sem Ramos Herrera <alu0100603194@ull.edu.es>
2013-12-11 16:19:43 +0000

    DSL funcionando con suma, resta, maximo y minimo

--- lib/p10lppt13DSL.rb
+++ lib/p10lppt13DSL.rb
@@ -1,4 +1,4 @@
-#require p10lppt13.rb
+require "p10lppt13.rb"
 
 class MatrizDSL
 
@@ -21,7 +21,7 @@ class MatrizDSL
 		@tipo << opc
 	end
 
-	def operando(matriz) #Funcion para la creacion de las matrices
+	def operando(nfil, ncol, matriz) #Funcion para la creacion de las matrices
 		aux = @tipo.pop
 		if (aux == "Densa")
 			m = Densa.new(nfil, ncol, matriz)
@@ -43,12 +43,52 @@ class MatrizDSL
 	end
 
 	def operador
-                case @op
-                   when "suma"
-                   	@result = @operandos[0]+@operandos[1]
-                   when "resta"
-                   	@result = @operandos[0]-@operandos[1]
-            	end                
+		case @op
+			when "suma"
+				@result = @operandos[0] + @operandos[1]
+			when "resta"
+				@result = @operandos[0] - @operandos[1]
+			when "max"
+				@result = @operandos[0].max
+			when "min"
+				@result = @operandos[0].min
+			end                
 	end
 
 end
+
+pruebaSumaMixta = MatrizDSL.new("suma") do
+	tipoMatriz "Densa"
+	operando 2,2,[[1,2],[3,4]]
+	tipoMatriz "Dispersa"
+	operando 2,2, 0 => { 0 => 2, 1 => 1 }
+end
+
+pruebaRestaMixta = MatrizDSL.new("suma") do
+	tipoMatriz "Densa"
+	operando 2,2,[[1,2],[3,4]]
+	tipoMatriz "Dispersa"
+	operando 2,2, 0 => { 0 => 2, 1 => 1 }
+end
+
+pruebaMax = MatrizDSL.new("max") do
+	tipoMatriz "Densa"
+	operando 2,2,[[1,20],[3,4]]
+end
+
+puts "Prueba Suma Matriz Densa con Dispersa"
+puts pruebaSumaMixta
+#puts "Prueba Suma Matrices Densas"
+#puts pruebaSumaDensa
+#puts "Prueba Suma Matrices Dispersas"
+#puts pruebaSumaDispersa
+
+puts "Prueba Resta Matriz Densa con Dispersa"
+puts pruebaRestaMixta
+#puts "Prueba Resta Matrices Densa"
+#puts pruebaRestaDensa
+#puts "Prueba Resta Matrices Dispersa"
+#puts pruebaRestaDispersa
+
+puts "Maximo de una Matriz"
+puts pruebaMax
