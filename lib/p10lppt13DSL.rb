da7aafb3710c5ad959345c9fe0aac660793f0182
José Alexander Delgado Hernandez <alu0100596403@ull.edu.es>
2013-12-11 15:34:10 +0000

    Añadiendo estructura del fichero DSL con clase y sus funciones

--- lib/p10lppt13DSL.rb
+++ lib/p10lppt13DSL.rb
@@ -1,6 +1,29 @@
+#require p10lppt13.rb
 
+class MatrizDSL
 
-class MatrizDSL < Matriz
+	attr_accessor :operandos, :op, :tipo, :result
 
+	def initialize(operador, &block) #Matriz DSL 
+		
+			
+	end
 
+	def to_s 
+
+	end
+
+	def tipoMatriz(opc)  #Funcion que ira almacenando en @modo los tipos de matrices
+		@tipo << opc
+	end
+
+	def operando(matriz) #Funcion para la creacion de las matrices
+	
+	end
+
+	def operador
+
+	end
+
+ 
 end
